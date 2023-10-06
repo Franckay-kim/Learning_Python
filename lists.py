a045c5f0e5b5bdb6bebe60110db05391c66dd030
Franckay-kim <fkim6322@gmail.com>
2023-10-07 00:03:03 +0300

    Lists, Tuples, Functions

---
+++ lists.py
@@ -0,0 +1,15 @@
+# creating a list of friends
+friends = ["Joyce", "Eric", "Lewis", "Tim"]
+print(friends)
+# printing a value in a list based of their index
+print(friends[2])
+# indexing backwards
+print(friends[-3])
+# prints from index one and the others after that
+print(friends[1:])
+# prints in the specified range (up to but not including 3)
+print(friends[1:3])
+# Modify a list
+friends[3] = "Timothy"
+print(friends[3])
+
