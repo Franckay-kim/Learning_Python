84db4738245f463114406ca58d0d5c36a84ed18d
Franckay-kim <fkim6322@gmail.com>
2023-10-10 00:04:07 +0300

    Classes and objects

---
+++ ChineseChef.py
@@ -0,0 +1,7 @@
+from Chef import Chef
+# Inheriting all functions from Chef class to ChineseChef
+class ChineseChef(Chef):
+
+
+    def make_fried_rice(self):
+        print("Makes fried rice")
---
+++ Classes and Objexts.py
@@ -0,0 +1,7 @@
+
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.2)
+student2 = Student("Pam", "Art", 3.6)
+
+print(student2.on_honor_roll())
